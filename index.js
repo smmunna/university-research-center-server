df51e143caa6e9b1d51fc6c7d539bc9654312532
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-06-15 14:18:46 +0600

    server created

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+//For using the MongoDB Cloud (online)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvrwrto.mongodb.net/?retryWrites=true&w=majority`;
+
+// For Running Locally
+// const uri = "mongodb://127.0.0.1:27017";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7) [Before Hosting , we should delete this line]
+        // await client.connect();
+
+        const database = client.db("researchDB");
+        const usersCollection = database.collection("users");
+        const allpapersCollection = database.collection("allpapers");
+
+        // Write down all of your routes 
+        // -----------------------------------------------------------------------------------------------------------------
+
+        // creating new user;
+        app.post('/users', async (req, res) => {
+            const users = req.body
+            const result = await usersCollection.insertOne(users)
+            res.send(result)
+        })
+
+        // Getting the single paper by id;
+        app.get('/allpapers/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await allpapersCollection.findOne(query)
+            res.send([result])
+        })
+
+        // Getting all the papers;
+        app.get('/allpapers', async (req, res) => {
+            const result = await allpapersCollection.find().toArray()
+            res.send(result)
+        })
+
+
+
+
+        // -----------------------------------------------------------------------------------------------------------------
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error (Removed this portion for solving the error)
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Our Research Center')
+})
+app.get('*', (req, res) => {
+    res.send('No routes Found')
+})
+
+
+app.listen(port, () => console.log('> Server is up and running on port : ' + port))
